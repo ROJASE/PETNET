fae93a7c22b604540835f21cc992ecd628094eb2
ROJASE <42590681+ROJASE@users.noreply.github.com>
2021-05-05 03:36:42 -0500

    Update Feeding

--- feeding_manager.js
+++ feeding_manager.js
@@ -35,17 +35,17 @@ var pin5 = 5;
 var pin6 = 6;
 
 // Potter Variables 
-var roomWidth = 999;
-var roomLength = 955;
-var ceilingHeight = 888;
+// var roomWidth = 999;
+// var roomLength = 955;
+// var ceilingHeight = 888;
 var restartAlgorithm = 0;
-var status = "NoneSt";
-var roomWidthReflected = "NoneRW";
-var roomLengthReflected = "NoneRL";
-var ceilingHeightReflected = "NoneCH";
+// var status = "NoneSt";
+// var roomWidthReflected = "NoneRW";
+// var roomLengthReflected = "NoneRL";
+// var ceilingHeightReflected = "NoneCH";
 var publish_request = 0;
-var timeToGreen = 1800;
-var timeToAmber = 300;
+// var timeToGreen = 1800;
+// var timeToAmber = 300;
 
 // My Variables 
 var pin7 = 7;
@@ -61,9 +61,15 @@ var pin12 = 12;
 var steps = 0;
 var latitude = 0;
 var longitude =0;
-var stepsReflected = "No Steps";
-var latitudeReflected = "No Latitude";
-var longitudeReflected = "No Longitude";
+
+// var stepsReflected = "No Steps";
+// var latitudeReflected = "No Latitude";
+// var longitudeReflected = "No Longitude";
+
+var foodWeight = 0;
+var time = 0;
+var duration = 0;
+
 var foodWeightReflect = "No Weight";
 var timeReflect = "No Time";
 var durationReflect = "No Duration"
@@ -102,20 +108,20 @@ function onConnect(socket)
     socket.on('loadParameters', handleLoadParameters);
 
     // set up callback for restartAlgorithm message
-    socket.on('restartAlgorithm', handleRestartAlgorithm);
+     socket.on('restartAlgorithm', handleRestartAlgorithm);
     
     // save the socket identifier in a variable
     soc = socket;
-    status = "N/A";
-    soc.emit("pinUpdate", '{"pin":"' + pin1 + '", "status":"' + status + '"}');
-    soc.emit("pinUpdate", '{"pin":"' + pin2 + '", "roomWidthReflected":"' + roomWidthReflected + '"}');
-    soc.emit("pinUpdate", '{"pin":"' + pin3 + '", "roomLengthReflected":"' + roomLengthReflected + '"}');
-    soc.emit("pinUpdate", '{"pin":"' + pin4 + '", "ceilingHeightReflected":"' + ceilingHeightReflected + '"}');
-    soc.emit("pinUpdate", '{"pin":"' + pin5 + '", "timeToAmber":"' + timeToAmber + '"}');
-    soc.emit("pinUpdate", '{"pin":"' + pin6 + '", "timeToGreen":"' + timeToGreen + '"}');
-    soc.emit("pinUpdate", '{"pin":"' + pin7 + '", "stepsReflected":"' + stepsReflected + '"}');
-    soc.emit("pinUpdate", '{"pin":"' + pin8 + '", "latitudeReflected":"' + latitudeReflected + '"}');
-    soc.emit("pinUpdate", '{"pin":"' + pin9 + '", "longitudeReflected":"' + longitudeReflected + '"}');
+    // status = "N/A";
+    // soc.emit("pinUpdate", '{"pin":"' + pin1 + '", "status":"' + status + '"}');
+    // soc.emit("pinUpdate", '{"pin":"' + pin2 + '", "roomWidthReflected":"' + roomWidthReflected + '"}');
+    // soc.emit("pinUpdate", '{"pin":"' + pin3 + '", "roomLengthReflected":"' + roomLengthReflected + '"}');
+    // soc.emit("pinUpdate", '{"pin":"' + pin4 + '", "ceilingHeightReflected":"' + ceilingHeightReflected + '"}');
+    // soc.emit("pinUpdate", '{"pin":"' + pin5 + '", "timeToAmber":"' + timeToAmber + '"}');
+    // soc.emit("pinUpdate", '{"pin":"' + pin6 + '", "timeToGreen":"' + timeToGreen + '"}');
+    // soc.emit("pinUpdate", '{"pin":"' + pin7 + '", "stepsReflected":"' + stepsReflected + '"}');
+    // soc.emit("pinUpdate", '{"pin":"' + pin8 + '", "latitudeReflected":"' + latitudeReflected + '"}');
+    // soc.emit("pinUpdate", '{"pin":"' + pin9 + '", "longitudeReflected":"' + longitudeReflected + '"}');
     soc.emit("pinUpdate", '{"pin":"' + pin10 + '", "foodWeightReflect":"' + foodWeightReflect + '"}');
     soc.emit("pinUpdate", '{"pin":"' + pin11 + '", "timeReflect":"' + timeReflect + '"}');
     soc.emit("pinUpdate", '{"pin":"' + pin12 + '", "durationReflect":"' + durationReflect + '"}');
@@ -130,24 +136,32 @@ function handleLoadParameters(data)
     // print the min and max to the console
     console.log('////////////////////////////////');
 
-    console.log('room width = ' + data.roomWidth);
-    console.log('room Length = ' + data.roomLength);
-    console.log('ceiling height = ' + data.ceilingHeight);
+    // console.log('room width = ' + data.roomWidth);
+    // console.log('room Length = ' + data.roomLength);
+    // console.log('ceiling height = ' + data.ceilingHeight);
 
-    console.log('Number of Sets = ' + data.steps);
-    console.log('Latitude Cordinate = ' + data.latitude);
-    console.log('Longitude Cordinate = ' + data.longitude);
+    // console.log('Number of Sets = ' + data.steps);
+    // console.log('Latitude Cordinate = ' + data.latitude);
+    // console.log('Longitude Cordinate = ' + data.longitude);
+
+    console.log('Food Weight = ' + data.Food_Weight);
+    console.log('Time = ' + data.Time);
+    console.log('Duration = ' + data.Duration);
 
 
     console.log('////////////////////////////////');
    
-    roomWidth = data.roomWidth;
-    roomLength = data.roomLength;
-    ceilingHeight = data.ceilingHeight;
+    // roomWidth = data.roomWidth;
+    // roomLength = data.roomLength;
+    // ceilingHeight = data.ceilingHeight;
 
-    steps = data.steps;
-    latitude = data.latitude;
-    longitude = data.longitude;
+    // steps = data.steps;
+    // latitude = data.latitude;
+    // longitude = data.longitude;
+
+    foodWeight = data.Food_Weight;
+    time = data.Time;
+    duration = data.Duration;
 
     publish_request = 1;
     console.log("Requesting PUBLISH");
@@ -307,31 +321,31 @@ function execute_session(connection, argv) {
                  
                    if(monitorRequested)
                    {
-	              roomWidthReflected = message.room_width_reflected;
-	              roomLengthReflected = message.room_length_reflected;
-	              ceilingHeightReflected = message.ceiling_height_reflected;
-                      status = message.status;
-                      timeToAmber = message.time_to_amber;
-	              timeToGreen = message.time_to_green;
-                  stepsReflected = message.steps;
-                  latitudeReflected = message.latitude;
-                  longitudeReflected = message.longitude;
+	            //   roomWidthReflected = message.room_width_reflected;
+	            //   roomLengthReflected = message.room_length_reflected;
+	            //   ceilingHeightReflected = message.ceiling_height_reflected;
+                //       status = message.status;
+                //       timeToAmber = message.time_to_amber;
+	            //   timeToGreen = message.time_to_green;
+                //   stepsReflected = message.steps;
+                //   latitudeReflected = message.latitude;
+                //   longitudeReflected = message.longitude;
                   foodWeightReflect = message.Food_Weight;
                   timeReflect = message.Time;
                   durationReflect = message.Duration;
                   
-	              console.log('Setting status to ' + message.status);
-                      console.log('status = ' + status);
+	            //   console.log('Setting status to ' + message.status);
+                //       console.log('status = ' + status);
 
-                      soc.emit("pinUpdate", '{"pin":"' + pin1 + '", "status":"' + status + '"}');
-                      soc.emit("pinUpdate", '{"pin":"' + pin2 + '", "roomWidthReflected":"' + roomWidthReflected + '"}');
-                      soc.emit("pinUpdate", '{"pin":"' + pin3 + '", "roomLengthReflected":"' + roomLengthReflected + '"}');
-                      soc.emit("pinUpdate", '{"pin":"' + pin4 + '", "ceilingHeightReflected":"' + ceilingHeightReflected + '"}');
-                      soc.emit("pinUpdate", '{"pin":"' + pin5 + '", "timeToAmber":"' + timeToAmber + '"}');
-                      soc.emit("pinUpdate", '{"pin":"' + pin6 + '", "timeToGreen":"' + timeToGreen + '"}');
-                      soc.emit("pinUpdate", '{"pin":"' + pin7 + '", "stepsReflected":"' + stepsReflected + '"}');
-                      soc.emit("pinUpdate", '{"pin":"' + pin8 + '", "latitudeReflected":"' + latitudeReflected + '"}');
-                      soc.emit("pinUpdate", '{"pin":"' + pin9 + '", "longitudeReflected":"' + longitudeReflected + '"}');
+                //       soc.emit("pinUpdate", '{"pin":"' + pin1 + '", "status":"' + status + '"}');
+                //       soc.emit("pinUpdate", '{"pin":"' + pin2 + '", "roomWidthReflected":"' + roomWidthReflected + '"}');
+                //       soc.emit("pinUpdate", '{"pin":"' + pin3 + '", "roomLengthReflected":"' + roomLengthReflected + '"}');
+                //       soc.emit("pinUpdate", '{"pin":"' + pin4 + '", "ceilingHeightReflected":"' + ceilingHeightReflected + '"}');
+                //       soc.emit("pinUpdate", '{"pin":"' + pin5 + '", "timeToAmber":"' + timeToAmber + '"}');
+                //       soc.emit("pinUpdate", '{"pin":"' + pin6 + '", "timeToGreen":"' + timeToGreen + '"}');
+                    //   soc.emit("pinUpdate", '{"pin":"' + pin7 + '", "stepsReflected":"' + stepsReflected + '"}');
+                    //   soc.emit("pinUpdate", '{"pin":"' + pin8 + '", "latitudeReflected":"' + latitudeReflected + '"}');
+                    //   soc.emit("pinUpdate", '{"pin":"' + pin9 + '", "longitudeReflected":"' + longitudeReflected + '"}');
                       soc.emit("pinUpdate", '{"pin":"' + pin10 + '", "foodWeightReflect":"' + foodWeightReflect + '"}');
                       soc.emit("pinUpdate", '{"pin":"' + pin11 + '", "timeReflect":"' + timeReflect + '"}');
                       soc.emit("pinUpdate", '{"pin":"' + pin12 + '", "durationReflect":"' + durationReflect + '"}');
@@ -343,32 +357,33 @@ function execute_session(connection, argv) {
                 });
 
                 yield connection.subscribe(argv.topic, aws_iot_device_sdk_v2_1.mqtt.QoS.AtLeastOnce, on_publish);
+
 		// TODO: this for loop just loops to a high value
 		// clearly, this is not a long-term solution, but 
 		// neither is this ugly webpage
-                for (let op_idx = 0; op_idx < 9999; ++op_idx) {
-                    const publish = () => __awaiter(this, void 0, void 0, function* () {
-                        const msg = {
-                            room_width: roomWidth,
-                            room_length: roomLength,
-                            ceiling_height: ceilingHeight,
-			    restart_algorithm: restartAlgorithm,
-                            sequence: op_idx + 1,
-                        };
+        //         for (let op_idx = 0; op_idx < 9999; ++op_idx) {
+        //             const publish = () => __awaiter(this, void 0, void 0, function* () {
+        //                 const msg = {
+        //                     room_width: roomWidth,
+        //                     room_length: roomLength,
+        //                     ceiling_height: ceilingHeight,
+		// 	    restart_algorithm: restartAlgorithm,
+        //                     sequence: op_idx + 1,
+        //                 };
 			    
-			restartAlgorithm = 0;
-                        const json = JSON.stringify(msg);
-			console.log("publish called");
-		        if(publish_request == 1) 
-			{
-			   console.log("&&&&&&&&&&&&& Got publish request &&&&&&&&&&&&&&");
-                           connection.publish('system_parameters', json, aws_iot_device_sdk_v2_1.mqtt.QoS.AtLeastOnce);
-			   console.log("****** PUBLISHED *******");
-			   publish_request = 0;
-		        }
-                    });
-                    setTimeout(publish, op_idx * 1000);
-		}
+		// 	restartAlgorithm = 0;
+        //                 const json = JSON.stringify(msg);
+		// 	console.log("publish called");
+		//         if(publish_request == 1) 
+		// 	{
+		// 	   console.log("&&&&&&&&&&&&& Got publish request &&&&&&&&&&&&&&");
+        //                    connection.publish('system_parameters', json, aws_iot_device_sdk_v2_1.mqtt.QoS.AtLeastOnce);
+		// 	   console.log("****** PUBLISHED *******");
+		// 	   publish_request = 0;
+		//         }
+        //             });
+        //             setTimeout(publish, op_idx * 1000);
+		// }
             }
             catch (error) {
                 reject(error);
